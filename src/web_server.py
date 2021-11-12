1c02028cb7b84457c4811b3cce514187f18c3f14
orazbaym <68743608+orazbaym@users.noreply.github.com>
2021-11-12 16:46:50 +0600

    Update web_server.py

--- src/web_server.py
+++ src/web_server.py
@@ -1,98 +1,141 @@
-from flask import Flask, render_template, redirect, url_for
-from flask.helpers import make_response
-from flask import request
-from flask.json import jsonify
-import sqlalchemy
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-import jwt
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thisismyflasksecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbCoin.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-url = "https://api.coinmarketcap.com/content/v3/news?coins=4030&page=1&size=5"
-
-data = {
-    'coins': '4030',
-    'page': '1',
-    'size': '5',
-    }
-
-Headers = {
-    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'origin': 'https://coinmarketcap.com',
-    'referer': 'https://coinmarketcap.com/',
-    'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform':'"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-site',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
-    }
-    
-def returnJsonRequest(url):
-    r = requests.get(url,headers = Headers,params = data)
-    return r.json()
-
-def returnTitle(json):
-    a = []
-    for i in json['data']:
-        a.append(i['meta']['title'])
-    return a
-
-def returnDesc(json):
-    a = []
-    for i in json['data']:
-        a.append(i['meta']['subtitle'])
-    return a
-
-def printPage(a,b):
-    for i in range(len(a)):
-        print("\033[1m" , a[i] , "\033[0m",'\n')
-        print("\t", b[i],'\n')
-        print(".......................................................................", '\n')
-    pass
-
-printPage(returnTitle(returnJsonRequest(url)),returnDesc(returnJsonRequest(url)))
-
 db = SQLAlchemy(app)
 
-class coins(db.Model):
+class coins:
+
+    def get_soup(self, coin_name):
+        url = "https://coinmarketcap.com/currencies/" + coin_name + "/news/"
+        options = webdriver.ChromeOptions()
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+        driver.get(url)
+        time.sleep(3)
+        for i in range(4):
+            driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+            time.sleep(3)
+            element = driver.find_element(By.XPATH, '//button[text()="Load More"]')
+            driver.execute_script("arguments[0].click();", element)
+            element.click()
+            time.sleep(3)
+
+        return BeautifulSoup(driver.page_source, "lxml")
+
+    def get_paragraphs(self, coin_name):
+        soup = self.get_soup(coin_name)
+        news = soup.find_all("div", {"class": "svowul-5 czQlor"})
+        ls = []
+        for i in news:
+            title = i.h3.text
+
+            body = ""
+            if (i.p):
+                body = i.p.text
+
+            link = i.a.get("href")
+            if (link.startswith("/headlines/news/")):
+                link = "https://coinmarketcap.com" + link
+
+            ls.append({"title": title, "body": body, "link": link})
+
+        return ls
+
+    def output_paragraphs(self, coin_name):
+        soup = self.get_soup(coin_name)
+        news = soup.find_all("div", {"class": "svowul-5 czQlor"})
+        num = 1
+        for i in news:
+            if (i.p):
+                print(str(num) + ")     Title : " + i.h3.text + ".\n    Link: " + i.a.get("href"))
+                print("Briefly: " + i.p.text)
+                print(
+                    "############################################################################################################")
+                num += 1
+
+
+
+
+
+
+class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True)
-    news = db.Column(db.String(1000), nullable=True)
-    
+    coin_name = db.Column(db.String)
+    title = db.Column(db.String)
+    body = db.Column(db.String)
+    link = db.Column(db.String)
+
+    def __init__(self, coin_name, title, body, link):
+        self.coin_name = coin_name
+        self.title = title
+        self.body = body
+        self.link = link
+
     def __repr__(self):
-        return f"<coins {self.id}>"
+        return '<Title %r>' % self.title
 
 
-@app.route('/')
+db.create_all()
 
-def Page():
 
-     if request.method == "POST":
-        # здесь должна быть проверка корректности введенных данных
-        try:
-            p = coins(name=request.form['name'], news=request.form['news'])
-            db.session.add(p)
-            db.session.commit()
-        except:
-            db.session.rollback()
-            print("Ошибка добавления в БД")
+
+
+
+
+@app.route("/coin", methods=['POST', 'GET'])
+def coin():
+    if request.method == 'POST':
+        coin_name = request.form['coin'].lower()
+
+        db_articles = News.query.filter_by(coin_name=coin_name).all()
+
+        if (db_articles):
+            return render_template('form.html', articles=db_articles)
+
+        Coins = coins()
+        articles = Coins.get_paragraphs(coin_name)
+
+        for article in articles:
+            db.session.add(News(coin_name, article['title'], article['body'], article['link']))
+
+        db.session.commit()
+
         return ''' 
-                <html>
+            <html>
                 <body>
                     <h1> Coin name <h1>
                     <form>
-                        <p><input type="text" name="coinName" /></p>
+                        <p><input type="text" name="name" /></p>
                         <p><input type="submit" value="CHECK" /></p>
                     </form>
                 </body>
             </html>'''
 
-if __name__ == '__main__':
-    
-    app.run(debug=True)
+    elif request.method == 'GET':
+        return ''' 
+            <html>
+                <body>
+                    <h1> Coin name <h1>
+                    <form>
+                        <p><input type="text" name="name" /></p>
+                        <p><input type="submit" value="CHECK" /></p>
+                    </form>
+                </body>
+            </html>'''
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
